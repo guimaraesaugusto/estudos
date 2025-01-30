997b5d21f22569804cbb253705d80a95bcf29d9b
guimaraesaugusto <guimaraesaug18@gmail.com>
2025-01-29 21:52:04 -0300

    Estudo da estrutura Json

---
+++ Json/index.js
@@ -0,0 +1,32 @@
+// Declarando e criando o Json
+let invoice = {
+	id: 12345,
+	name: "Augusto",
+    age: 24,
+    products: {
+    0: ["notebook", 2599.90],
+    1: ["teclado", 129.90],
+    2: ["Monitor", 899.90]
+    },
+    jobTittle: "Analista"
+}
+
+// criando função para processar o Json
+function processingInvoice(Jsoninvoice){
+	console.log(`O comprador é ${Jsoninvoice.name} e sua idade é ${Jsoninvoice.age}`)
+	console.log(`-----------------------`)
+	console.log(`Produtos solicitados:`)
+
+	for(index = 0; index < Object.keys(Jsoninvoice.products).length; index ++){
+	let i = 0
+	let j = 0
+    // Jsoninvoice.products[index][0] = busca o 1 item do json no caso é o item
+	// Jsoninvoice.products[index][1] = busca o 2 item do json no caso é o preço
+    console.log(`- ${Jsoninvoice.products[index][0]}: R$ ${Jsoninvoice.products[index][1]}`)
+    }
+
+}
+
+//chamando a função com base no Json
+processingInvoice(invoice)
+
