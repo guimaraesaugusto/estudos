7d133edfe2e70076bb3112228a659693241aa09f
guimaraesaugusto <guimaraesaug18@gmail.com>
2025-01-29 22:04:40 -0300

    Estudo da estrututa Json com exemplo de como percorrer e processar todos os dados contidos

--- Json/index.js
+++ Json/index.js
@@ -3,7 +3,7 @@ let invoice = {
 	id: 12345,
 	name: "Augusto",
     age: 24,
-    products: {
+    products: { //json dentro de json
     0: ["notebook", 2599.90],
     1: ["teclado", 129.90],
     2: ["Monitor", 899.90]
@@ -24,6 +24,14 @@ function processingInvoice(Jsoninvoice){
 	// Jsoninvoice.products[index][1] = busca o 2 item do json no caso é o preço
     console.log(`- ${Jsoninvoice.products[index][0]}: R$ ${Jsoninvoice.products[index][1]}`)
     }
+
+   /* //existe um for especifico para percorrer json, no caso abaixo iremos percorrer o json products que está dentro do json invoive
+
+    for(let index in invoice.products) {
+        let [productName, productPrice] = invoice.products[index] //declara a variavel em forma de array e coloca os valores do array dentro de invoice.products conforme o index dos itens
+        console.log(`- ${productName}: R$ ${productPrice}`)
+    } 
+    */
 
 }
 
