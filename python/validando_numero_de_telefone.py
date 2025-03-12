5f692b83b1918aa1f7360f6a0e359614ab5f594f
guimaraesaugusto <guimaraesaug18@gmail.com>
2025-03-12 20:43:43 -0300

    Atualizando estudos Python

--- python/validando_numero_de_telefone.py
+++ python/validando_numero_de_telefone.py
@@ -5,3 +5,4 @@ if all(caractere in permitidos for caractere in telefone):
     print("Obrigado por cadastrar seu número!")
 else:
     print("Número inválido! Use apenas números, hífens, parênteses e espaços.")
+
---
+++ python/validando_numero_par_impar.py
@@ -0,0 +1,6 @@
+numero = int(input("Insira um número inteiro: "))
+
+if numero % 2 == 0:
+    print(f'''O número {numero} é par''')
+else:
+    print(f'''O número {numero} é ímpar''')
